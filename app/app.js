e43a9cfca91f9277780f8ace4997e9cec28b3792
Nataly Menares <nataly.menares@gmail.com>
2018-10-07 10:09:34 -0700

    Add quote

--- app/app.js
+++ app/app.js
@@ -1,32 +1,51 @@
+document.addEventListener("DOMContentLoaded", getAPI);
+let default_content = document.getElementById("tl-default");
+
+function getAPI(e) {
+  fetch("https://quotes.rest/qod")
+    .then(response => response.json())
+    .then(data => default_content.innerHTML = `${data.contents.quotes[0].quote} - ${data.contents.quotes[0].author}`)
+};
+
 const name = document.getElementById("name");
 const natalyModal = document.getElementById("natalyModal");
 const span = document.getElementById("close");
 
 name.addEventListener("click", openModal);
-window.addEventListener("click", closeModal2)
+window.addEventListener("click", closeModal2);
 
 function openModal(e) {
   e.preventDefault();
   natalyModal.style.display = "block";
-}
+};
 
 function closeModal2(e) {
   if(e.target === natalyModal || e.target === span){
     natalyModal.style.display = "none";
-  }  
-}
+  }
+};
 
 // timeline
 const timeline = document.getElementById("timeLine");
 timeline.addEventListener("click", openYear);
 
+const tl_content = document.getElementById("timeline-content")
+
 
 function openYear(e){
   e.preventDefault();
-  e.target.children[1].children[0].style.display= "block";
-  e.target.children[1].children[1].style.display= "block";
-
-}
+  Array.from(timeline.children).forEach(el => {
+    if (el === e.target) { el.classList.add("selected");} 
+    else { el.classList.remove("selected");}
+  });  
+  
+  let id = e.target.id;
+  let content = document.getElementById(`tl-${id}`);
+  Array.from(tl_content.children).forEach (el => {
+    if (el === content) { el.classList.remove("hidden");}
+    else { el.classList.add("hidden");}
+  });  
+};
 
 
 
