31e75a51aac57d6e6117f4b5b58c2f096ee00d12
Nataly Menares <nataly.menares@gmail.com>
2023-01-08 19:23:27 -0800

    update navbar

--- app/app.js
+++ app/app.js
@@ -1,209 +1,208 @@
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
 
-// navbar
-const navbar = document.getElementById("navBar");
-const myName = document.getElementById("name");
-const burguer = document.getElementById("burguer");
-const menuList = document.getElementById("menu");
-const content = document.getElementById("content");
-
-myName.onmouseover = function() {
-  myName.children[0].innerHTML = "Resume";
-  myName.onmouseout = function() {
-    myName.children[0].innerHTML = "Nataly Menares";
-  };
-};
-
-//mobilemenu
-myName.addEventListener("touchstart", openMenu_1, false);
-let counter = 0;
-
-function openMenu_1(e){
-  e.preventDefault();
-  myName.addEventListener("touchmove", e => e.preventDefault(), false);
-  myName.addEventListener("touchend", openMenu_2, false);
-  const touchObj1 = e.changedTouches[0];
-  const startMenuX = touchObj1.pageX;
-  const startMenuY = touchObj1.pageY;
-
-  function openMenu_2(ev){
-    ev.preventDefault();
-    const touchObj2 = ev.changedTouches[0];
-    const endMenuX = touchObj2.pageX;
-    const endMenuY = touchObj2.pageY;
-    const distMenuX = Math.abs(startMenuX - endMenuX); 
-    const distMenuY = Math.abs(startMenuY - endMenuY);
-
-    if (distMenuX < 2 && distMenuY < 2){  
-      window.scrollTo(0, 0);
-      closeMenu();
-      counter = 0;
-    };
-  };
-};
-
-burguer.onclick = function(e) {
-  e.preventDefault();
-  if (window.scrollY > 0) { counter = 0 };
-  counter += 1;
-  window.scrollTo(0, 0);
-  if (counter === 1) {
-    openMenu();
-  } else {
-    closeMenu();
-    counter = 0;
-  };
-};
+// burguer behavior: Click opens/closes menu
+const burguer = document.getElementsByClassName("burguer")[0];
+const verticalMenu = document.getElementsByClassName("vertical-menu")[0];
+const overlay = document.getElementsByClassName("overlay")[0];
+let menuIsOpen = false;
 
 function openMenu() {
-  navbar.style.position = "sticky";
-  navbar.style.top = "0px";
-  menuList.style.display = "flex";
-  content.style.marginTop = "40px";
-};
+  overlay.classList.remove("hide-overlay");
+  overlay.classList.add("show-overlay");
+  verticalMenu.style.display = "flex";
+  verticalMenu.style.flexDirection = "column";
+  menuIsOpen = !menuIsOpen;
+}
 
 function closeMenu() {
-  navbar.style.position = "fixed";
-  menuList.style.display = "none";
-  content.style.marginTop = "80px";
-};
-
-//canvas
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-ctx.beginPath();
-ctx.fillStyle = "#ffffff";
-ctx.rect(0, 0, 800, 500);
-ctx.fill();
-ctx.font = "100px Megrim";
-
-setTimeout(write, 1);
-
-function write(){
-  ctx.fillStyle = "#252839";
-  ctx.textBaseline = "top";
-  ctx.fillText("Think Out", 180, 40);
-  ctx.fillText("Develop", 200, 140);
-  ctx.fillText("Test", 290, 240);
-  ctx.fillText("Optimize", 200, 340);
+  overlay.classList.remove("show-overlay");
+  overlay.classList.add("hide-overlay");
+  verticalMenu.style.display = "none";
+  menuIsOpen = !menuIsOpen;
 }
 
-canvas.addEventListener("mousemove", makeTransparent, true);
+window.addEventListener("resize", closeMenu);
 
-function makeTransparent(e){
+burguer.onclick = (e) => {
   e.preventDefault();
-  let pos_x = e.screenX;
-  let pos_y = e.screenY;
-  ctx.globalCompositeOperation = "destination-out";
-  ctx.fillStyle = "#0066FF";
-  ctx.beginPath();
-  ctx.arc(pos_x - 200, pos_y - 200, 300, 0, 2 * Math.PI, true);
-  ctx.fill(); 
-}
+  if (menuIsOpen) closeMenu();
+  else openMenu();
+};
 
-// timeline
-const timeline = document.getElementById("timeLine");
-timeline.addEventListener("click", openYear, false);
-let id,
-    contentTimeLine
-
-const tl_content = document.getElementById("timeline-content");
-
-function openYear(e){
+overlay.onclick = (e) => {
   e.preventDefault();
-  id = e.target.id;
-  year = document.getElementById(`${id}`);
-  contentTimeLine = document.getElementById(`tl-${id}`);
-
-  Array.from(timeline.children).forEach(el => {
-    if (el === year) { el.classList.add("selected") } 
-    else { el.classList.remove("selected"); }
-  });  
-    
-  Array.from(tl_content.children).forEach (el => {
-    if (el === contentTimeLine) {
-      el.classList.remove("hidden");
-    } else {
-      el.classList.add("hidden");
-    }
-  });  
+  closeMenu();
 };
 
-//swipe timeline on mobile
-tl_content.addEventListener("touchstart", handleStartTouching, false);
+// const navbar = document.getElementById("navBar");
+// const myName = document.getElementById("name");
 
-let startX,
-    startY,
-    distX,
-    distY,
-    elapsedTime,
-    startTime
+// const menuList = document.getElementById("menu");
+// const content = document.getElementById("content");
 
-const threshold = 50,
-      allowedTime = 200
+// //mobilemenu
+// myName.addEventListener("touchstart", openMenu_1, false);
+// let counter = 0;
 
-function handleStartTouching(e){
-  tl_content.addEventListener("touchmove", e => e.preventDefault(), false);
-  tl_content.addEventListener("touchend", handleEndTouching, false);
-  e.preventDefault();
-  const touchObj = e.changedTouches[0];
-  distX = 0;
-  startX = touchObj.pageX;
-  startY = touchObj.pageY;
-  startTime = new Date().getTime();
-};
+// function openMenu_1(e) {
+//   e.preventDefault();
+//   myName.addEventListener("touchmove", (e) => e.preventDefault(), false);
+//   myName.addEventListener("touchend", openMenu_2, false);
+//   const touchObj1 = e.changedTouches[0];
+//   const startMenuX = touchObj1.pageX;
+//   const startMenuY = touchObj1.pageY;
 
-function handleEndTouching(e){
-  e.preventDefault();
-  const touchObj = e.changedTouches[0];
-  distX = touchObj.pageX - startX;
-  distY = touchObj.pageY - startY;
-  elapsedTime = new Date().getTime() - startTime;
-  const rightSwipeBol = (elapsedTime <= allowedTime && Math.abs(distX) >= threshold);
-  if(rightSwipeBol) { handleSwipe(); }
-  else if(Math.abs(distY) > 5) { activeScroll(); };
-};
+//   function openMenu_2(ev) {
+//     ev.preventDefault();
+//     const touchObj2 = ev.changedTouches[0];
+//     const endMenuX = touchObj2.pageX;
+//     const endMenuY = touchObj2.pageY;
+//     const distMenuX = Math.abs(startMenuX - endMenuX);
+//     const distMenuY = Math.abs(startMenuY - endMenuY);
 
-function handleSwipe() {
-  if (year) {
-    if (distX > 0 && year.id !== "2012") {
-      changeYear(-1);
-    } else if (distX < 0 && year.id !== "2018") {
-      changeYear(1);
-    };
-  };
-};
+//     if (distMenuX < 2 && distMenuY < 2) {
+//       window.scrollTo(0, 0);
+//       closeMenu();
+//       counter = 0;
+//     }
+//   }
+// }
 
-function changeYear(x) {
-  year.classList.remove("selected");
-  contentTimeLine.classList.add("hidden");
-  id = (parseInt(year.id, 10) + x).toString();
-  year = document.getElementById(`${id}`);
-  contentTimeLine = document.getElementById(`tl-${id}`);
-  year.classList.add("selected");
-  contentTimeLine = document.getElementById(`tl-${id}`);
-  contentTimeLine.classList.remove("hidden");
-};
+// //canvas
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
 
-function activeScroll(e){
-    window.scrollBy({left: 0, top: (- 2 * distY) , behavior: "smooth"});
-};
+// ctx.beginPath();
+// ctx.fillStyle = "#ffffff";
+// ctx.rect(0, 0, 800, 500);
+// ctx.fill();
+// ctx.font = "100px Megrim";
 
-//quote
-document.addEventListener("DOMContentLoaded", getAPI, true);
-const default_content = document.getElementById("quote");
-const quote = document.createElement("p");
+// setTimeout(write, 1);
 
-function getAPI(e) {
-  fetch("https://quotes.rest/qod")
-    .then(response => response.json())
-    .then(data => default_content.appendChild(quote).innerHTML = `"${data.contents.quotes[0].quote}" - ${data.contents.quotes[0].author}`)
-};
+// function write() {
+//   ctx.fillStyle = "#252839";
+//   ctx.textBaseline = "top";
+//   ctx.fillText("Think Out", 180, 40);
+//   ctx.fillText("Develop", 200, 140);
+//   ctx.fillText("Test", 290, 240);
+//   ctx.fillText("Optimize", 200, 340);
+// }
 
+// function makeTransparent(e) {
+//   e.preventDefault();
+//   let pos_x = e.screenX;
+//   let pos_y = e.screenY;
+//   ctx.globalCompositeOperation = "destination-out";
+//   ctx.fillStyle = "#0066FF";
+//   ctx.beginPath();
+//   ctx.arc(pos_x - 200, pos_y - 200, 300, 0, 2 * Math.PI, true);
+//   ctx.fill();
+// }
 
+// // timeline
+// const timeline = document.getElementById("timeLine");
+// timeline.addEventListener("click", openYear, false);
+// let id, contentTimeLine;
 
+// const tl_content = document.getElementById("timeline-content");
 
+// function openYear(e) {
+//   e.preventDefault();
+//   id = e.target.id;
+//   year = document.getElementById(`${id}`);
+//   contentTimeLine = document.getElementById(`tl-${id}`);
 
+//   Array.from(timeline.children).forEach((el) => {
+//     if (el === year) {
+//       el.classList.add("selected");
+//     } else {
+//       el.classList.remove("selected");
+//     }
+//   });
+
+//   Array.from(tl_content.children).forEach((el) => {
+//     if (el === contentTimeLine) {
+//       el.classList.remove("hidden");
+//     } else {
+//       el.classList.add("hidden");
+//     }
+//   });
+// }
+
+// //swipe timeline on mobile
+// tl_content.addEventListener("touchstart", handleStartTouching, false);
+
+// let startX, startY, distX, distY, elapsedTime, startTime;
+
+// const threshold = 50,
+//   allowedTime = 200;
+
+// function handleStartTouching(e) {
+//   tl_content.addEventListener("touchmove", (e) => e.preventDefault(), false);
+//   tl_content.addEventListener("touchend", handleEndTouching, false);
+//   e.preventDefault();
+//   const touchObj = e.changedTouches[0];
+//   distX = 0;
+//   startX = touchObj.pageX;
+//   startY = touchObj.pageY;
+//   startTime = new Date().getTime();
+// }
+
+// function handleEndTouching(e) {
+//   e.preventDefault();
+//   const touchObj = e.changedTouches[0];
+//   distX = touchObj.pageX - startX;
+//   distY = touchObj.pageY - startY;
+//   elapsedTime = new Date().getTime() - startTime;
+//   const rightSwipeBol =
+//     elapsedTime <= allowedTime && Math.abs(distX) >= threshold;
+//   if (rightSwipeBol) {
+//     handleSwipe();
+//   } else if (Math.abs(distY) > 5) {
+//     activeScroll();
+//   }
+// }
+
+// function handleSwipe() {
+//   if (year) {
+//     if (distX > 0 && year.id !== "2012") {
+//       changeYear(-1);
+//     } else if (distX < 0 && year.id !== "2018") {
+//       changeYear(1);
+//     }
+//   }
+// }
+
+// function changeYear(x) {
+//   year.classList.remove("selected");
+//   contentTimeLine.classList.add("hidden");
+//   id = (parseInt(year.id, 10) + x).toString();
+//   year = document.getElementById(`${id}`);
+//   contentTimeLine = document.getElementById(`tl-${id}`);
+//   year.classList.add("selected");
+//   contentTimeLine = document.getElementById(`tl-${id}`);
+//   contentTimeLine.classList.remove("hidden");
+// }
+
+// function activeScroll(e) {
+//   window.scrollBy({ left: 0, top: -2 * distY, behavior: "smooth" });
+// }
+
+// //quote
+// document.addEventListener("DOMContentLoaded", getAPI, true);
+// const default_content = document.getElementById("quote");
+// const quote = document.createElement("p");
+
+// function getAPI(e) {
+//   fetch("https://quotes.rest/qod")
+//     .then((response) => response.json())
+//     .then(
+//       (data) =>
+//         (default_content.appendChild(
+//           quote
+//         ).innerHTML = `"${data.contents.quotes[0].quote}" - ${data.contents.quotes[0].author}`)
+//     );
+// }
