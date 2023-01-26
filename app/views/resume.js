27ce08e300c648ca16cde5a89e1bfff456c28e66
Nataly Menares <nataly.menares@gmail.com>
2023-01-25 16:35:02 -0800

    add resume part 1

--- app/views/resume.js
+++ app/views/resume.js
@@ -1,3 +1,34 @@
 const resume = `
-  <div>Resume</div>
+<div class="resume-wrapper">
+  <div class="resume">
+    <div class="resume-details">
+      <h1>
+        <span class="highlight">Fullstack Developer</span> @MikMak, New York, NY, US.
+        <ul>
+          <li>From June 2021 to January 2023.</li>
+          <li>Vue.js, d3.js, Jest, vuetify, REST api.</li>
+        </ul>
+      </h1>
+      <h1>
+        <span class="highlight">Frontend Developer</span> @Lore IO, Sunnyvale, CA, US.
+        <ul>
+          <li>From January 2019 to June 2021.</li>
+          <li>Ember.js, semantic-ui, Websocket API.</li>
+        </ul>
+      </h1>
+      <h1><span class="highlight">Marketing Engineer</span> @SimpliRoute, Santiago, Chile.
+        <ul>
+          <li>From January 2017 to May 2018.</li>
+          <li>Google Analytics, Tableau, MySql.</li>
+        </ul>
+      </h1>
+    </div>
+    <div class="resume-box">skills</div>
+  </div>
+  <div class="seemore">
+    <a href="public/cvResume.pdf" target="_blank" alt="resume">
+      <img src="https://nmenares.github.io/portfolio/public/resume.png">
+    </a>
+  </div>
+</div>
 `;
